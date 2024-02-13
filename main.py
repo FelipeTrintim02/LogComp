95afed24eed9003d27f9d48d748c1226185093f2
Felipe Menotti Trintim <felipemt3@al.insper.edu.br>
2024-02-13 00:32:40 -0300

    adicionando main.py

---
+++ main.py
@@ -0,0 +1,49 @@
+import sys
+
+class Calculator:
+    def __init__(self):
+        pass
+
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+    
+    def calculate(self, expression):
+        expression = expression.replace(' ','')
+        number = []
+        operation = []
+        num = ''
+
+        for i in expression:
+            if i.isdigit():
+                num += i
+            else:
+                number.append(int(num))
+                operation.append(i)
+                num = ''
+        
+        if num:
+            number.append(int(num))
+
+        result = number[0]
+
+        for i, op in enumerate(operation):
+            if op == "+":
+                result = self.add(result, number[i+1])
+            elif op == "-":
+                result = self.subtract(result, number[i+1])
+
+        return result    
+
+if __name__ == "__main__":
+
+    if len(sys.argv) > 1:
+        expression = sys.argv[1]
+    else:
+        expression = ''
+
+    result = Calculator().calculate(expression)
+
+    print(result)
