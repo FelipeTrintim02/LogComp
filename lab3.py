3c5cd63ee9bcff6650365ec583dd275ed83bf347
Felipe Menotti Trintim <felipemt3@al.insper.edu.br>
2024-03-11 17:09:07 -0300

    lab4

--- lab3.py
+++ lab3.py
@@ -1,21 +1,13 @@
 import sys
-from abc import abstractmethod
 
 class Token:
     def __init__(self, type, value):
         self.type = type
         self.value = value
 
-class PrePro:
-    @staticmethod
-    def filter(expression):
-        whitoutComments = expression.split("--")[0]
-        return whitoutComments
-
 class Tokenizer:
     def __init__(self, source):
-        filtered_source = PrePro.filter(source)
-        self.source = filtered_source
+        self.source = source
         self.position = 0
         self.next = None
 
@@ -53,53 +45,6 @@ class Tokenizer:
             sys.stderr.write(f"Unexpected character: {self.source[self.position]}\n")
             sys.exit(1)
 
-class Node:
-    def __init__(self, value : int, children):
-        self.value = value
-        self.children = children
-
-    @abstractmethod
-    def evaluate(self):
-        pass
-
-class BinOp(Node):
-    def __init__(self, value, children):
-        super().__init__(value, children)
-
-    def evaluate(self):
-        if self.value == '+':
-            return self.children[0].evaluate() + self.children[1].evaluate()
-        elif self.value == '-':
-            return self.children[0].evaluate() - self.children[1].evaluate()
-        elif self.value == '*':
-            return self.children[0].evaluate() * self.children[1].evaluate()
-        elif self.value == '/':
-            return self.children[0].evaluate() // self.children[1].evaluate()
-        
-class UnOp(Node):
-    def __init__(self, value, children):
-        super().__init__(value, children)
-
-    def evaluate(self):
-        if self.value == '+':
-            return self.children[0].evaluate()
-        elif self.value == '-':
-            return -self.children[0].evaluate()
-        
-class IntVal(Node):
-    def __init__(self, value):
-        super().__init__(value, [])
-
-    def evaluate(self):
-        return self.value
-    
-class NoOp(Node):
-    def __init__(self):
-        super().__init__(None, [])
-
-    def evaluate(self):
-        pass
-
 class Parser:
     @staticmethod
     def parseExpression(tokenizer):
@@ -107,10 +52,10 @@ class Parser:
         while tokenizer.next.type in ['PLUS', 'MINUS']:
             if tokenizer.next.type == 'PLUS':
                 tokenizer.selectNext()
-                result = BinOp('+', [result, Parser.parseTerm(tokenizer)])
+                result += Parser.parseTerm(tokenizer)
             elif tokenizer.next.type == 'MINUS':
                 tokenizer.selectNext()
-                result = BinOp('-', [result, Parser.parseTerm(tokenizer)])
+                result -= Parser.parseTerm(tokenizer)
         return result
 
     @staticmethod
@@ -119,10 +64,10 @@ class Parser:
         while tokenizer.next.type in ['MULT', 'DIV']:
             if tokenizer.next.type == 'MULT':
                 tokenizer.selectNext()
-                result = BinOp('*', [result, Parser.parseFactor(tokenizer)])
+                result *= Parser.parseFactor(tokenizer)
             elif tokenizer.next.type == 'DIV':
                 tokenizer.selectNext()
-                result = BinOp('/', [result, Parser.parseFactor(tokenizer)])
+                result //= Parser.parseFactor(tokenizer)
         return result
 
     @staticmethod
@@ -130,13 +75,13 @@ class Parser:
         if tokenizer.next.type == 'INT':
             result = tokenizer.next.value
             tokenizer.selectNext()
-            return IntVal(result)
+            return result
         elif tokenizer.next.type == 'PLUS':
             tokenizer.selectNext()
-            return UnOp('+', [Parser.parseFactor(tokenizer)])
+            return Parser.parseFactor(tokenizer)
         elif tokenizer.next.type == 'MINUS':
             tokenizer.selectNext()
-            return UnOp('-', [Parser.parseFactor(tokenizer)])
+            return -Parser.parseFactor(tokenizer)
         elif tokenizer.next.type == 'LPAREN':
             tokenizer.selectNext()
             result = Parser.parseExpression(tokenizer)
@@ -149,38 +94,21 @@ class Parser:
             sys.stderr.write(f"Expected number or (expression)\n")
             sys.exit(1)
 
-
     @staticmethod
     def run(code):
         tokenizer = Tokenizer(code)
         tokenizer.selectNext()  # Initialize the tokenizer
         result = Parser.parseExpression(tokenizer)
-        result = result.evaluate()
         if tokenizer.next.type != 'EOF':
             sys.stderr.write("Unexpected tokens after expression\n")
             sys.exit(1)
         return result
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        sys.stderr.write("Usage: python main.py <filename.lua>\n")
-        sys.exit(1)
-
-    filename = sys.argv[1]
-
-    if not filename.endswith(".lua"):
-        sys.stderr.write("Error: File extension must be .lua\n")
-        sys.exit(1)
-
+    expression = sys.argv[1]
     try:
-        with open(filename, 'r') as file:
-            code = file.read()
-
-        result = Parser.run(code)
+        result = Parser.run(expression)
         print(result)
-    except FileNotFoundError:
-        sys.stderr.write(f"Error: File {filename} not found\n")
-        sys.exit(1)
     except Exception as e:
         sys.stderr.write(f"Error: {e}\n")
         sys.exit(1)
